fb31797d908b98d7b1f805fe76e7db57c1b7e0ee
João Victor <jvmestrerj18@gmail.com>
2025-03-24 16:52:16 -0300

    Mensagem da Atualização

--- sistema/views.py
+++ sistema/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Aqui irão ficar todas as views ( controladores ) ref ao app sistema
+# A Funçaõ index informa o que vai acontecer 
+def index(request):
+    return render(
+        request,
+        'sistema/index.html',
+    )
+
+
